e126f07ea5704d876aee99f653bbfc7bbe48bedb
masanag <masaki.nagaishi@gmail.com>
2020-04-27 23:00:00 +0900

    finished 7

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hey ninjas!!')
+  home: Home()
 ));
 
-
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'my first app',
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text(
+          'Hello, ninjas!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click!'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
