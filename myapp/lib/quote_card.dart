8d8f2ca03604f2d42cdf5a6da4409f5647fd53d5
masanag <masaki.nagaishi@gmail.com>
2020-04-30 17:48:19 +0900

    finished 21st

--- myapp/lib/quote_card.dart
+++ myapp/lib/quote_card.dart
@@ -3,7 +3,8 @@ import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
   final Quote quote;
-  QuoteCard({ this.quote });
+  final Function delete;
+  QuoteCard({ this.quote, this.delete });
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +29,13 @@ class QuoteCard extends StatelessWidget {
                 fontSize: 14.0,
                 color: Colors.grey[800],
               ),
-            )
+            ),
+            SizedBox(height: 8.0),
+            FlatButton.icon(
+              onPressed: delete,
+              icon: Text('delete quote'),
+              label:Icon(Icons.delete),
+            ),
           ],
         ),
       ),
